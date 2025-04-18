95bcdf313f4cece9149244e9987823717364c98b
nazim1971 <md.nazimuddinaj@gmail.com>
2025-04-18 18:21:47 +0600

    add vercel tp

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { Server } from "http";
 import app from "./app";
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-async function main() {
-    const server: Server = app.listen(port, ()=>{
-        console.log("Server is running on port", port);
-    })
+if (process.env.NODE_ENV !== "production") {
+  const server: Server = app.listen(port, () => {
+    console.log(`🚀 Server is running on http://localhost:${port}`);
+  });
 }
 
-main();
+export default app;
